7a609a33e1593715be37a557f35890345f9264aa
Kim yonggwan <sssw0101@naver.com>
2022-03-09 00:50:44 +0900

    프로그래머스 - 문자열 다루기 기본

---
+++ src/programmers/Programmers_12918.java
@@ -0,0 +1,25 @@
+package programmers;
+
+// https://programmers.co.kr/learn/courses/30/lessons/12918
+// 연습문제 > 문자열 다루기 기본
+public class Programmers_12918 {
+    public static void main(String[] args) {
+        boolean answer = true;
+        answer = solution("a234");
+        answer = solution("1234");
+    }
+
+    static boolean solution(String s){
+        if(s.length() != 4 && s.length() != 6){
+            return false;
+        }
+
+        for(int i = 0; i<s.length(); i++){
+            if(s.charAt(i) < '0' || s.charAt(i) > '9'){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
