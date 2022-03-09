2b8e24c201f916bb16f0121440274711aa4dfa80
Kim yonggwan <sssw0101@naver.com>
2022-03-09 16:29:01 +0900

    프로그래머스 - K번째수

---
+++ src/programmers/Programmers_42748.java
@@ -0,0 +1,34 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class Programmers_42748 {
+    public static void main(String[] args) {
+        int[] answer = {};
+//        answer = solution(new int[]{1,5,2,6,3,7,4}, new int[][]{{2,5,3}});
+        answer = solution(new int[]{1,5,2,6,3,7,4}, new int[][]{{2,5,3},{4,4,1},{1,7,3}});
+        for(int num : answer){
+            System.out.println(num);
+        }
+    }
+
+    static int[] solution(int[] array, int[][] commands){
+        int[] answer = new int[commands.length];
+
+        for(int i = 0; i< commands.length; i++){
+            int first =  commands[i][0] -1;
+            int second = commands[i][1] -1;
+            int third = commands[i][2]- 1;
+            int length = second - first + 1;
+
+            int[] tempArray = new int[length];
+            for(int j = 0; j<length; j++){
+                tempArray[j] = array[first+j];
+            }
+            Arrays.sort(tempArray);
+            answer[i] = tempArray[third];
+        }
+
+        return answer;
+    }
+}
