08742458d4e9452616bde6038b91eb208473be06
Kim yonggwan <sssw0101@naver.com>
2022-03-10 00:18:29 +0900

    프로그래머스 - 하샤드 수

---
+++ src/programmers/Programmers_12947.java
@@ -0,0 +1,28 @@
+package programmers;
+
+// https://programmers.co.kr/learn/courses/30/lessons/12947
+// 연습문제 > 하샤드 수
+public class Programmers_12947 {
+    public static void main(String[] args) {
+        boolean answer = true;
+        answer = solution(11);
+        System.out.println(answer);
+    }
+
+    static boolean solution(int x){
+        boolean answer = true;
+
+        int temp = x;
+        int sum = 0;
+        while(temp != 0){
+            sum += temp % 10;
+
+            temp = temp / 10;
+        }
+        if(x % sum != 0){
+            answer = false;
+        }
+
+        return answer;
+    }
+}
