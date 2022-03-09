d317b8c4f7816f9125c3eb80424b68cb8871b568
Kim yonggwan <sssw0101@naver.com>
2022-03-09 16:57:11 +0900

    프로그래머스 - 문자열을 정수로 바꾸기

---
+++ src/programmers/Programmers_12925.java
@@ -0,0 +1,39 @@
+package programmers;
+
+// https://programmers.co.kr/learn/courses/30/lessons/12925
+// 연습문제 > 문자열을 정수로 바꾸기
+
+public class Programmers_12925 {
+    public static void main(String[] args) {
+        int answer = 0;
+        answer = solution("1234");
+        answer = getStrToInt("-1234");
+        System.out.println(answer);
+    }
+
+    static int solution(String s){
+        int answer = 0;
+
+        char first = s.charAt(0);
+        if(first >= '0' || first <= '9'){
+            answer = Integer.parseInt(s);
+        }
+
+        return answer;
+    }
+
+    // 다른 사람 정답
+    static int getStrToInt(String str) {
+        boolean Sign = true;
+        int result = 0;
+
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '-')
+                Sign = false;
+            else if(ch !='+')
+                result = result * 10 + (ch - '0');
+        }
+        return Sign?1:-1 * result;
+    }
+}
