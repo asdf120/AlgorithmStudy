0ff78180168016f923bcf6d17d6e88fb7de4d1e6
Kim yonggwan <sssw0101@naver.com>
2022-03-28 00:09:11 +0900

    백준 - 단지번호붙이기

--- src/bakjoon/B_2667.java
+++ src/bakjoon/B_2667.java
@@ -5,78 +5,71 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-/**
- * https://www.acmicpc.net/problem/2667
- * 단지번호붙이기
- */
-public class B_2667 {
-    public static void main(String[] args) throws IOException {
-//        solution(7, new int[][]{{0, 1, 1, 0, 1, 0, 0}, {0, 1, 1, 0, 1, 0, 1}, {1, 1, 1, 0, 1, 0, 1}, {0, 0, 0, 0, 1, 1, 1},
-//                {0, 1, 0, 0, 0, 0, 0}, {0, 1, 1, 1, 1, 1, 0}, {0, 1, 1, 1, 0, 0, 0}});
+    /**
+     * https://www.acmicpc.net/problem/2667
+     * 단지번호붙이기
+     */
+    public class B_2667 {
+        static int[][] houseArr = {};   //지도
+        static int count = 0;   //배열 갯수
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        static int houseNum = 0;    //단지
+        static int houseCount = 0;  //집의 수
 
-        count = Integer.parseInt(br.readLine());
-        int[][] arr = new int[count][count];
+        public static void main(String[] args) throws IOException {
 
-        for(int i=0; i<count; i++) {
-            String[] input = br.readLine().split("");
-            for(int j=0; j<count; j++) {
-                arr[i][j] = Integer.parseInt(input[j]);
-            }
-        }
-        solution(count, arr);
-    }
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    static int[][] houseArr = {};
-    static int count = 0;
+            count = Integer.parseInt(br.readLine());
+            houseArr = new int[count][count];
 
-    static int houseNum = 0;
-    static int houseCount = 0;
-
-    static void solution(int num, int[][] arr) {
-        houseArr = arr;
-        count = num;
-
-        Map<Integer, Integer> map = new HashMap<>();
-
-        for (int i = 0; i < count; i++) {
-            for (int j = 0; j < count; j++) {
-                if (dfs(i, j)) {
-                    houseNum++;
-                    map.put(houseNum, houseCount);
+            for (int i = 0; i < count; i++) {
+                String[] input = br.readLine().split("");
+                for (int j = 0; j < count; j++) {
+                    houseArr[i][j] = Integer.parseInt(input[j]);
                 }
-                houseCount = 0;
+            }
+
+            Map<Integer, Integer> map = new HashMap<>(); //1
+
+            //2
+            for (int i = 0; i < count; i++) {
+                for (int j = 0; j < count; j++) {
+                    if (dfs(i, j)) { //3
+                        houseNum++;
+                        map.put(houseNum, houseCount);
+                    }
+                    houseCount = 0; //4
+                }
+            }
+
+            List<Integer> answerArr = new ArrayList<>();
+            for (int n : map.values()) {
+                answerArr.add(n);
+            }
+            //5
+            Collections.sort(answerArr);
+
+            System.out.println(houseNum);
+            for (int n : answerArr) {
+                System.out.println(n);
             }
         }
 
-        List<Integer> answerArr = new ArrayList<>();
-        for (int n : map.values()){
-            answerArr.add(n);
-        }
-        Collections.sort(answerArr);
-
-        System.out.println(houseNum);
-        for(int n : answerArr){
-            System.out.println(n);
-        }
-
-    }
-
-    static boolean dfs(int x, int y) {
-        if (x < 0 || x >= count || y < 0 || y >= count) {
+        static boolean dfs(int x, int y) {
+            if (x < 0 || x >= count || y < 0 || y >= count) {
+                return false;
+            }
+            if (houseArr[x][y] == 1) {
+                houseCount++;
+                houseArr[x][y] = 0;
+                dfs(x, y + 1); // 동
+                dfs(x, y - 1); // 서
+                dfs(x + 1, y); // 남
+                dfs(x - 1, y); // 북
+                return true;
+            }
             return false;
         }
-        if (houseArr[x][y] == 1) {
-            houseCount++;
-            houseArr[x][y] = 0;
-            dfs(x, y + 1); // 동
-            dfs(x, y - 1); // 서
-            dfs(x + 1, y); // 남
-            dfs(x - 1, y); // 북
-            return true;
-        }
-        return false;
-    }
 
 }
