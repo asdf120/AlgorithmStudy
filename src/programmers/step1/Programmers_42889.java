40bfda0e15053cf9c4373331c47052b2cf0aaf4d
Kim yonggwan <sssw0101@naver.com>
2022-04-07 01:17:12 +0900

    프로그래머스 - 실패율

--- src/programmers/step1/Programmers_42889.java
+++ src/programmers/step1/Programmers_42889.java
@@ -8,9 +8,11 @@ import java.util.*;
 public class Programmers_42889 {
     public static void main(String[] args) {
         int[] answer = {};
-        answer = otherSolution(5, new int[]{2, 1, 2, 6, 2, 4, 3, 3});
+//        answer = solution(5, new int[]{2, 1, 2, 6, 2, 4, 3, 3});
+        answer = solution(4, new int[]{4,4,4,4,4});
+//        answer = otherSolution(5, new int[]{2, 1, 2, 6, 2, 4, 3, 3});
 
-        for(int num : answer){
+        for (int num : answer) {
             System.out.print(num + " ");
         }
     }
@@ -18,40 +20,68 @@ public class Programmers_42889 {
     static int[] solution(int N, int[] stages) {
         int[] answer = new int[N];
 
-        Map<Integer, List<Integer>> failStageMap = new HashMap<>();
-
-        // 실패 스테이지 별로 담음
-        for (int i = 0; i < stages.length; i++) {
-            List<Integer> failList = failStageMap.getOrDefault(stages[i], null);
-            if (failList == null) {
-                failList = new ArrayList<>();
-            }
-            failList.add(stages[i]);
-            failStageMap.put(stages[i], failList);
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        Map<Integer, Double> failMap = new HashMap<>();
+        for (int i = 1; i <= N; i++) {
+            map.put(i, new ArrayList<>());
+            failMap.put(i, 0.0);
         }
-        System.out.println("26line");
-        System.out.println(((double) 1 / 8));
 
-
-        for (List<Integer> stage : failStageMap.values()) {
-            for(int i = 0; i<stage.size(); i++){
-                System.out.print(stage.get(i) + " ");
+        // 스테이지별 실패 카운트를 맵에 담음
+        for (int stage : stages) {
+            if (stage == N + 1) {
+                continue;
             }
-            System.out.println();
+            List<Integer> list = map.get(stage);
+            list.add(list.size() + 1);
+        }
+        
+        // 스테이지별 남은 도전자 수
+        int size = stages.length;
+        
+        // 단계별 실패율을 맵에 담음
+        for (Integer stage : map.keySet()) {
+            double failCount = map.get(stage).size();
+            if (failCount == 0) {
+                failMap.put(stage, 0.0);
+                continue;
+            }
+            failMap.put(stage, failCount / size);
+            size = size - (int) failCount;
+        }
+
+        // 실패율 리스트를 생성후 리스트에 실패율을 담은 후 내림차순
+        List<Double> failList = new ArrayList<>();
+        for (Double d : failMap.values()) {
+            failList.add(d);
+        }
+        Collections.sort(failList, Collections.reverseOrder());
+
+        // 단계별 실패율을 담은 맵의 값과 실패율 리스트를 비교하여 asnwer에 넣음
+        for (int i = 0; i < failList.size(); i++) {
+            int stage = 0;
+            for (int n : failMap.keySet()) {
+                if (failMap.get(n) == failList.get(i)) {
+                    answer[i] = n;
+                    stage = n;
+                    break;
+                }
+            }
+            failMap.remove(stage);
         }
 
         return answer;
     }
 
-    
+
     // https://sujin-k.tistory.com/61 참고
-    static int[] otherSolution(int N, int[] stages){
+    static int[] otherSolution(int N, int[] stages) {
         int[] answer = new int[N];
 
-        double[] stage = new double[N+1];
+        double[] stage = new double[N + 1];
 
-        for(int i : stages){
-            if(i == N+1){
+        for (int i : stages) {
+            if (i == N + 1) {
                 continue;
             }
             stage[i]++;
@@ -64,13 +94,13 @@ public class Programmers_42889 {
         double num = stages.length;
         double temp = 0;
 
-        for(int i = 1; i<stage.length; i++){
+        for (int i = 1; i < stage.length; i++) {
             temp = stage[i];
 
             if (num == 0) {
                 stage[i] = 0;
-            }else{
-                stage[i] = stage[i]/num;    // 실패 확률
+            } else {
+                stage[i] = stage[i] / num;    // 실패 확률
                 num = num - temp;
             }
 
@@ -80,9 +110,9 @@ public class Programmers_42889 {
 
         Collections.sort(failList, Collections.reverseOrder());
 
-        for(int i = 0; i< failList.size(); i++){
-            for(int j = 1; j<stage.length; j++){
-                if(failList.get(i) == stage[j]){
+        for (int i = 0; i < failList.size(); i++) {
+            for (int j = 1; j < stage.length; j++) {
+                if (failList.get(i) == stage[j]) {
                     answer[i] = j;
                     stage[j] = -1;
 
