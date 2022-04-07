ffe0aff5a9de110713891b9697edec8339838230
Kim yonggwan <sssw0101@naver.com>
2022-04-08 01:46:42 +0900

    프로그래머스(2022 KAKAO BLIND RECRUITMENT) - 신고 결과 받기
    2022 KAKAO BLIND RECRUITMENT

--- src/programmers/step1/Programmers_92334.java
+++ src/programmers/step1/Programmers_92334.java
@@ -8,9 +8,11 @@ public class Programmers_92334 {
 
     public static void main(String[] args) {
         int[] answer = {};
-        answer = solution(new String[]{"muzi", "frodo", "apeach", "neo"}, new String[]{"muzi frodo", "apeach frodo", "frodo neo",
-                        "muzi neo", "apeach muzi"}, 2);
-//        answer = solution(new String[]{"con", "ryan"}, new String[]{"ryan con", "ryan con", "ryan con", "ryan con"}, 3);
+//        answer = solution(new String[]{"muzi", "frodo", "apeach", "neo"}, new String[]{"muzi frodo", "apeach frodo", "frodo neo",
+//                        "muzi neo", "apeach muzi"}, 2);
+//        answer = reSolution(new String[]{"muzi", "frodo", "apeach", "neo"}, new String[]{"muzi frodo", "apeach frodo", "frodo neo",
+//                "muzi neo", "apeach muzi"}, 2);
+        answer = reSolution(new String[]{"con", "ryan"}, new String[]{"ryan con", "ryan con", "ryan con", "ryan con"}, 3);
         for(int i : answer){
             System.out.print(i + " ");
         }
@@ -50,6 +52,43 @@ public class Programmers_92334 {
 
         for(int i = 0; i<idList.length; i++){
             answer[i] = reportCountMap.getOrDefault(idList[i],0);
+        }
+
+        return answer;
+    }
+
+    static int[] reSolution(String[] idList, String[] report, int k){
+        int[] answer = new int[idList.length];
+
+        Set<String> set = new HashSet<>();
+        for(String reportedId : report){
+            set.add(reportedId);
+        }
+
+        Map<String, List<String>> reportedMap = new HashMap<>();
+        for(String re : set){
+            String[] reports = re.split(" ");
+            List<String> reportList = reportedMap.getOrDefault(reports[1], null);
+            if (reportList == null) {
+                reportList = new ArrayList<>();
+            }
+            reportList.add(reports[0]);
+            reportedMap.put(reports[1], reportList);
+        }
+
+        Map<String,Integer> reportMap = new HashMap<>();
+        for(String key : reportedMap.keySet()){
+            int reportedCount = reportedMap.get(key).size();
+            if (reportedCount >= k) {
+                for(String id : reportedMap.get(key)){
+                    int count = reportMap.getOrDefault(id, 0)+1;
+                    reportMap.put(id, count);
+                }
+            }
+        }
+
+        for(int i = 0; i< idList.length; i++){
+            answer[i] = reportMap.getOrDefault(idList[i], 0);
         }
 
         return answer;
