5fba6453e992773ff263c0a4f9b6ec37a65f32c3
Kim yonggwan <sssw0101@naver.com>
2023-06-11 18:36:20 +0900

    [unrated] Title: 개인정보 수집 유효기간, Time: 5.00 ms, Memory: 66.5 MB -BaekjoonHub

--- 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/unrated/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -5,76 +5,34 @@ class Solution {
         List<Integer> answerList = new ArrayList<>();
 
         String[] todayArray = today.split("\\.");
+        int todayDay = Integer.parseInt(todayArray[0]) * 12 * 28 + Integer.parseInt(todayArray[1]) * 28 + Integer.parseInt(todayArray[2]);
 
         for(int i = 0; i< privacies.length; i++){
-            String signUpDate = privacies[i].split(" ")[0];
-            String termType = privacies[i].split(" ")[1];
+            String[] privacyArray = privacies[i].split(" ");
+            String signUpDate = privacyArray[0];
+            String termType = privacyArray[1];
 
             String[] signUpDateArray = signUpDate.split("\\.");
+            int signUpDateDay = Integer.parseInt(signUpDateArray[0]) * 12 * 28 + Integer.parseInt(signUpDateArray[1]) * 28 + Integer.parseInt(signUpDateArray[2]);
 
-            if(!checkValidation(terms, todayArray, signUpDateArray, termType)){
-                answerList.add(i+1);
-            }
-        }
+            for(String term : terms){
+                String termName = term.split(" ")[0];
+                int periodTime = Integer.parseInt(term.split(" ")[1]) * 28;
 
-        int[] answerArray = answerList.stream()
-                        .mapToInt(Integer::intValue)
-                        .toArray();
-        return answerArray;
-    }
-    
-    boolean checkValidation(String[] terms, String[] todayArray, String[] signUpDateArray, String termType){
-        for(String term: terms){
-            String termName = term.split(" ")[0];
-            int termPeriod = Integer.parseInt(term.split(" ")[1]);
-
-            if(termType.equals(termName)){
-                int todayYear = Integer.parseInt(todayArray[0]);
-                int todayMonth = Integer.parseInt(todayArray[1]);
-                int todayDay = Integer.parseInt(todayArray[2]);
-
-                int signUpYear = Integer.parseInt(signUpDateArray[0]);
-                int signUpMonth = Integer.parseInt(signUpDateArray[1]);
-                int signUpDay = Integer.parseInt(signUpDateArray[2]);
-
-                int year = termPeriod / 12;
-                int month = termPeriod % 12;
-                signUpYear += year;
-                signUpMonth += month;
-                
-                if(signUpMonth > 12){
-                    signUpMonth %= 12;
-                    signUpYear += 1;
-                }
-
-                if(signUpDay == 1){
-                    signUpDay = 28;
-                    signUpMonth -= 1;
-                    if(signUpMonth == 0){
-                        signUpYear -= 1;
-                        signUpMonth = 12;
+                if(termType.equals(termName)){
+                    if(todayDay >= signUpDateDay + periodTime){
+                        answerList.add(i+1);
                     }
-                }else{
-                    signUpDay -= 1;
+                    break;
                 }
 
-                if(signUpYear < todayYear){
-                    return false;
-                }else if(signUpYear > todayYear){
-                    return true;
-                }
-
-                if(signUpMonth < todayMonth){
-                    return false;
-                }else if(signUpMonth > todayMonth){
-                    return true;
-                }
-
-                if(signUpDay < todayDay){
-                    return false;
-                }
             }
         }
-        return true;
+        int[] answerArray = answerList.stream()
+                .mapToInt(Integer::intValue)
+                .toArray();
+
+
+        return answerArray;
     }
 }
