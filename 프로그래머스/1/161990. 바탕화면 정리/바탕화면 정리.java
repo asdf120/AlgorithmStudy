0ca29e427f0494ef3368874ebdb6f2ba161cf177
Kim yonggwan <sssw0101@naver.com>
2023-10-17 23:00:25 +0900

    [level 1] Title: 바탕화면 정리, Time: 22.68 ms, Memory: 99.8 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -2,31 +2,23 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] wallpaper) {
+        int[] answer = {51,51,-1,-1};
         int height = wallpaper.length;
         int width = wallpaper[0].split("").length;
-
-        List<Integer> xList = new ArrayList<>();
-        List<Integer> yList = new ArrayList<>();
 
         for(int i = 0; i<height; i++){
             for(int j = 0; j<width; j++){
                 if(wallpaper[i].split("")[j].equals("#")){
-                    xList.add(i);
-                    yList.add(j);
+                    answer[0] = Math.min(answer[0], i);
+                    answer[1] = Math.min(answer[1], j);
+                    answer[2] = Math.max(answer[2], i);
+                    answer[3] = Math.max(answer[3], j);
                 }
             }
         }
+        answer[2] += 1;
+        answer[3] += 1;
 
-        Collections.sort(xList);
-        Collections.sort(yList);
-
-        int minX = xList.get(0);
-        int minY = yList.get(0);
-        int maxX = xList.get(xList.size()-1)+1;
-        int maxY = yList.get(yList.size()-1)+1;
-
-
-        
-        return new int[]{minX, minY, maxX, maxY};
+        return answer;
     }
 }
