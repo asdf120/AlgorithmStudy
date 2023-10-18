18c6efe1732d04f9a888c6146cc8102c1e009daa
Kim yonggwan <sssw0101@naver.com>
2023-10-18 23:43:33 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,21 +1,15 @@
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {int c1 = 0;
         int c2 = 0;
-        for(String g: goal){
-
-            if(c1 < cards1.length){
-                if(cards1[c1].equals(g)){
-                    c1++;
-                    continue;
-                }
+        for (String g : goal) {
+            if (c1 < cards1.length && cards1[c1].equals(g)) {
+                c1++;
+                continue;
             }
-            if(c2 < cards2.length){
-                if(cards2[c2].equals(g)){
-                    c2++;
-                    continue;
-                }
+            if (c2 < cards2.length && cards2[c2].equals(g)) {
+                c2++;
+                continue;
             }
-
             return "No";
         }
 
